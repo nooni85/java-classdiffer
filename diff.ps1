5ceaecf1b3bee469f1b1940bba5ac107888c60b9
Sangwoo Joo <nooni85@naver.com>
2023-04-04 02:27:44 +0900

    Modified diff

--- diff.ps1
+++ diff.ps1
@@ -7,14 +7,29 @@ function writeDiff {
         $targetName
     )
 
+    if (Test-Path -Path $targetName -PathType Leaf) {
+        Remove-Item $targetName
+    }
+
+    $cont=""
+
     Get-ChildItem $path -Recurse | 
         Foreach-Object {
             if (Test-Path -Path $_.FullName -PathType Leaf) {
                 # $content = Get-Content $_.FullName
-                $result = md5deep64 $_.FullName
-                Add-Content $targetName $result
+                $ext =[System.IO.Path]::GetExtension($_.FullName)
+                if($ext -eq ".class") {
+                    $javap = javap -sysinfo $_.FullName
+                    $cont += (($javap -match "SHA-256 checksum") -replace "SHA-256 checksum").Trim() + "  " + $_.FullName
+                    $cont += "`r`n"
+                } else {
+                    $cont += md5deep64 $_.FullName
+                    $cont += "`r`n"
+                }
             }
         }
+    $result = $cont.replace($path, '')
+    Add-Content $targetName $result
 }
 
 writeDiff -path $DIFF1 -targetName 'diff2.txt'
